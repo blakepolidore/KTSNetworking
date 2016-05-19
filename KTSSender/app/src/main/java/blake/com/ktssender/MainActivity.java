7826497ba6e6fcb0d2bf60efd843e73e47196561
Blake Polidore <blakepolidore@gmail.com>
2016-05-19 09:49:20 -0700

    added spinners and adapters

--- KTSSender/app/src/main/java/blake/com/ktssender/MainActivity.java
+++ KTSSender/app/src/main/java/blake/com/ktssender/MainActivity.java
@@ -2,12 +2,53 @@ package blake.com.ktssender;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText nameEditText;
+    private Spinner monthSpinner;
+    private Spinner daySpinner;
+    private Spinner yearSpinner;
+    private Button submitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setViews();
+        setSpinners();
     }
+
+    private void setViews() {
+        nameEditText = (EditText) findViewById(R.id.name_edit_text);
+        monthSpinner = (Spinner) findViewById(R.id.month_spinner);
+        daySpinner = (Spinner) findViewById(R.id.day_spinner);
+        yearSpinner = (Spinner) findViewById(R.id.year_spinner);
+        submitButton = (Button) findViewById(R.id.send_button);
+    }
+
+    private void setSpinners() {
+        ArrayAdapter<CharSequence> adapterMonth = ArrayAdapter.createFromResource(this,
+                R.array.months, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapterDay = ArrayAdapter.createFromResource(this,
+                R.array.days, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapterYear = ArrayAdapter.createFromResource(this,
+                R.array.years, android.R.layout.simple_spinner_item);
+        adapterMonth.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapterDay.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapterYear.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        monthSpinner.setAdapter(adapterMonth);
+        daySpinner.setAdapter(adapterDay);
+        yearSpinner.setAdapter(adapterYear);
+        monthSpinner.setPrompt("Select Month");
+        daySpinner.setPrompt("Select Day");
+        yearSpinner.setPrompt("Select Year");
+    }
+
+    
 }
