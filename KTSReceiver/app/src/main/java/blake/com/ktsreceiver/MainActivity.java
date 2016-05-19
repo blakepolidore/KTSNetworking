cae8587fea4608345abd5f1c2d92515e7067f87f
Blake Polidore <blakepolidore@gmail.com>
2016-05-19 10:20:12 -0700

    added views to java

--- KTSReceiver/app/src/main/java/blake/com/ktsreceiver/MainActivity.java
+++ KTSReceiver/app/src/main/java/blake/com/ktsreceiver/MainActivity.java
@@ -2,12 +2,25 @@ package blake.com.ktsreceiver;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView name;
+    private TextView birthday;
+    private TextView age;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setViews();
+    }
+
+    private void setViews() {
+        name = (TextView) findViewById(R.id.name_text);
+        birthday = (TextView) findViewById(R.id.birthday_text);
+        age = (TextView) findViewById(R.id.age_text);
     }
 }
