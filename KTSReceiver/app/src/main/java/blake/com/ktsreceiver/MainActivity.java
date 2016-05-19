fb972f84c083312962e2687c30442edf169a3d37
Blake Polidore <blakepolidore@gmail.com>
2016-05-19 11:04:47 -0700

    before unmarshalling error

--- KTSReceiver/app/src/main/java/blake/com/ktsreceiver/MainActivity.java
+++ KTSReceiver/app/src/main/java/blake/com/ktsreceiver/MainActivity.java
@@ -1,8 +1,11 @@
 package blake.com.ktsreceiver;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,11 +19,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         setViews();
+        getDataFromIntent();
     }
 
     private void setViews() {
         name = (TextView) findViewById(R.id.name_text);
         birthday = (TextView) findViewById(R.id.birthday_text);
         age = (TextView) findViewById(R.id.age_text);
+    }
+
+    private void getDataFromIntent() {
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        String type = intent.getType();
+
+        if (Intent.ACTION_SEND_MULTIPLE.equals(action) && type != null) {
+            if (type.startsWith("text/")) {
+                handleIntent(intent);
+            }
+        }
+    }
+
+    private void handleIntent(Intent intent) {
+        ArrayList<PersonalInfo> personalInfoList = intent.getParcelableArrayListExtra(Intent.EXTRA_TEXT);
+        if (personalInfoList != null) {
+            PersonalInfo personalInfo = personalInfoList.get(0);
+            name.setText(personalInfo.name);
+            String fullBirthday = personalInfo.month + "/" + personalInfo.day + "/" + personalInfo;
+            birthday.setText(fullBirthday);
+        }
     }
 }
